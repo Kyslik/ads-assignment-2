a905eb3de61e5265f05586a0cc3ac50dcbb554fb
Martin Kiesel <martin.kiesel@gmail.com>
2016-03-28 13:49:46 +0200

    init Xcode project

---
+++ ads-assignment-2/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  ads-assignment-2
+//
+//  Created by Martin Kiesel on 28/03/16.
+//  Copyright © 2016 Martin Kiesel. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
